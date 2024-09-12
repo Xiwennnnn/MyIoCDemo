7fb81cbd16b1d7a47c532b3c3d84c932d3915137
root <2077353357@qq.com>
2024-09-12 18:08:29 +0800

    second

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         XiwenContext ctx = XiwenBootStart.run(Main.class);
         UserService userService = (UserService)ctx.getBean("UserService");
+        OrderService orderService = (OrderService)ctx.getBean("OrderService");
+        System.out.println(userService.orderService);
+        System.out.println(orderService.userService);
     }
 }
